e81c76a3e887f65f0ad5d22e90f5ff8a21c4c033
yariy <sidonypunjek@hotmail.com>
2025-08-04 17:54:52 +0300

    Ver 1.0

---
+++ start.bat
@@ -0,0 +1,13 @@
+﻿@echo off
+cd /d %~dp0
+
+echo 🔹 Активируем виртуальное окружение...
+call venv\Scripts\activate.bat
+
+echo 🔹 Устанавливаем зависимости (на всякий случай)...
+pip install -r requirements.txt
+
+echo 🔹 Запускаем Telegram-бота...
+python main.py
+
+pause
